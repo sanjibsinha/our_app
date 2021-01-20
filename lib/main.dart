4fc922c550951c854e07801e5b35cf859d8aad56
Sanjib <sanjib12sinha@gmail.com>
2021-01-20 07:10:27 +0530

    understanding onpress method

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'controller/inherited-widget/inherited_widget_on_top.dart';
+// import 'controller/inherited-widget/inherited_widget_on_top.dart';
 
-main() => runApp(OurApp());
+void main() {
+  runApp(OurApp());
+}
 
 class OurApp extends StatelessWidget {
   @override
@@ -10,8 +12,105 @@ class OurApp extends StatelessWidget {
       title: 'Our App',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: InheritedWidgetOnTop(),
+        body: ACenterClass(),
       ),
     );
   }
 }
+
+class ACenterClass extends StatelessWidget {
+  void pressRemote() {
+    print('Remote has been pressed.');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        width: 350.00,
+        height: 350.00,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          border: Border.all(
+            color: Colors.deepOrange,
+            width: 2.0,
+            style: BorderStyle.solid,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black54,
+              blurRadius: 20.0,
+              spreadRadius: 20.0,
+            ),
+          ],
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Colors.red,
+              Colors.white,
+            ],
+          ),
+// to make shape active we need to comment out borderRadius property and vice versa
+          //shape: BoxShape.circle,
+        ),
+        child: Column(
+          children: [
+            Text(
+              'Press',
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            RaisedButton(
+              child: Text(
+                'Press Button',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.blue,
+                ),
+              ),
+              onPressed: pressRemote,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/**
+ * 
+class OurScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // It creates a piece of material dsign
+    return Material(
+      // Column is a vertical, linear layout.
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Text(
+                'Hello, world!',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.redAccent,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+ */
